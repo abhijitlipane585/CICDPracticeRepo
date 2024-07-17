62d1e73307ed8bf3c7494ec2f858cc9eb08433ba
abhijitlipane585 <162475552+abhijitlipane585@users.noreply.github.com>
2024-07-17 19:36:37 +0530

    Update Example.java

--- Java_Programs/Example.java
+++ Java_Programs/Example.java
@@ -2,6 +2,6 @@ public class Example
    {
          public static void main(String []args)
                   {
-                      System.out.println("Hello Abhijit ,You are running Java code ");
+                      System.out.println("Hello Abhijit Lipane ,You are running Java code ");
                   }
     }
