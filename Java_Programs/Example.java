afe7beb39dd0738d5cf59e8e88ef88c5f0d8b4ac
Abhijit <abhijitlipane585@gmail.com>
2024-07-17 18:06:54 +0530

    First Commit

---
+++ Java_Programs/Example.java
@@ -0,0 +1,7 @@
+public class Example 
+   {
+         public static void main(String []args)
+                  {
+                      System.out.println("Hello Abhijit ,You are running Java code ");
+                  }
+    }
